86ea1fc2b22932b2497323cf7b9b67518ab68fcd
hihilla <hilla.guz@gmail.com>
2020-09-23 13:43:53 +0200

    add confirm prompt

--- cmd/shipperctl/cmd/decommission.go
+++ cmd/shipperctl/cmd/decommission.go
@@ -1,18 +1,21 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
-	"k8s.io/client-go/kubernetes"
+	"io"
 	"sort"
 	"strings"
+
+	"github.com/spf13/cobra"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 
 	"github.com/bookingcom/shipper/cmd/shipperctl/configurator"
 	cmdutil "github.com/bookingcom/shipper/cmd/shipperctl/util"
 	shipper "github.com/bookingcom/shipper/pkg/apis/shipper/v1alpha1"
 	shipperclientset "github.com/bookingcom/shipper/pkg/client/clientset/versioned"
 	releaseutil "github.com/bookingcom/shipper/pkg/util/release"
-	"github.com/spf13/cobra"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const (
@@ -157,4 +160,30 @@ func collectReleases(kubeClient kubernetes.Interface, shipperClient shipperclien
 		}
 	}
 	return deleteReleases, updateAnnotations, fmt.Errorf(strings.Join(errList, ","))
+}
+
+// askForConfirmation asks the user for confirmation. A user must type in "y" or "n" and
+// then press enter. It has fuzzy matching, so "y", "Y" both count as
+// confirmations. If the input is not recognized, it will ask again. The function does not return
+// until it gets a valid response from the user.
+// taken from https://gist.github.com/r0l1/3dcbb0c8f6cfe9c66ab8008f55f8f28b
+func askForConfirmation(stdin io.Reader, message string) (bool, error) {
+	reader := bufio.NewReader(stdin)
+
+	for {
+		fmt.Printf("%s [y/n]: ", message)
+
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			return false, err
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		if response == "y" {
+			return true, nil
+		} else if response == "n" {
+			return false, nil
+		}
+	}
 }
